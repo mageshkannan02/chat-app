57e3d38706744ca20a07ada9217c7e60975f62a5
mageshkannan02 <166358763+mageshkannan02@users.noreply.github.com>
2025-02-25 15:39:56 +0530

    modifed path

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,3 @@
-// app.ts
 const express = require("express");
 const authRoute = require("../routes/auth.route");
 const messageRoute = require("../routes/message.route");
@@ -8,32 +7,34 @@ const { connectDB } = require("../lib/db");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const path=require("path");
+const path = require("path");
 
+const __dirname = path.resolve(); // Keep only this declaration
 
 app.use(cookieParser());
-app.use(express.json())
-app.use(cors({
-    origin:"http://localhost:5173"
-,
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],}))
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 const port = process.env.PORT;
-const __dirname = path.resolve()
-app.use(bodyParser.json())
-app.use("/api/auth", authRoute);  
-app.use("/api/message", messageRoute);  
 
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend","/dist","/index.html"));
-    });
+app.use(bodyParser.json());
+app.use("/api/auth", authRoute);
+app.use("/api/message", messageRoute);
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-
 server.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
-    connectDB()
+  console.log(`Server is running on port ${port}`);
+  connectDB();
 });
